776860c332b25011f443e4ec91d60d49c3411d65
William Tshani <williamtshani@gmail.com>
2025-09-14 23:25:49 +0200

    Completed W02 assignment

---
+++ scripts/temples.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+    const nameHeader = document.querySelector('.nameHeader'); // Select the h1 element
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+        nameHeader.classList.toggle('hidden'); // Toggle the 'hidden' class
+    });
+});
+
+
+
+// Footer information
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+const copyrightYearElement = document.getElementById('currentyear');
+const lastModifiedElement = document.getElementById('lastModified');
+copyrightYearElement.textContent = currentYear;
+lastModifiedElement.textContent = `Last update: ${lastModified}`;
