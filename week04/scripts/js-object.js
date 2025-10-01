6285af2ed34cc7e7e78e07bc720fa1b79e4269cd
William Tshani <williamtshani@gmail.com>
2025-10-01 05:55:28 +0200

    Completed it

---
+++ week04/scripts/js-object.js
@@ -0,0 +1,37 @@
+let acourse = {
+    code: "WDD131",
+    title: "Dynamic Web Fundamentals",
+    credits: 2,
+    sections: [
+        {
+            section: "001",
+            enrolled: 95,
+            instructor: "Robert Diaz Rodriguez"
+        },
+        {
+            section: "002",
+            enrolled: 80,
+            instructor: "Sarah Gobble"
+        }
+    ]
+};
+
+function setCourseInformation(course) {
+    document.querySelector("#courseName").innerHTML = `${course.code} - ${course.title}`;
+}
+
+function sectionTemplate(section) {
+    return `<tr>
+                <td>${section.sectionNumber}</td>
+                <td>${section.enrolled}</td>
+                <td>${section.instructor}</td>
+            <tr>`;
+}
+
+function renderSections(sections) {
+  const html = sections.map(sectionTemplate);
+  document.querySelector("#sections tbody").innerHTML = html.join("");
+}
+
+setCourseInformation(aCourse);
+renderSections(aCourse.sections);
