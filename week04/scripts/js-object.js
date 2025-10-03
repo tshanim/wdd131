079638ae8b6a16aa23e577577c680ae0bdd45842
William Tshani <williamtshani@gmail.com>
2025-10-03 11:48:38 +0200

    Completed the JS Objects assignment

--- week04/scripts/js-object.js
+++ week04/scripts/js-object.js
@@ -1,25 +1,29 @@
-let acourse = {
+// Add sections property with two section objects
+let aCourse = {
     code: "WDD131",
     title: "Dynamic Web Fundamentals",
     credits: 2,
     sections: [
         {
-            section: "001",
+            sectionNumber: "001",
             enrolled: 95,
             instructor: "Robert Diaz Rodriguez"
         },
         {
-            section: "002",
+            sectionNumber: "002",
             enrolled: 80,
             instructor: "Sarah Gobble"
         }
     ]
 };
 
+// Set course information to existing HTML element with and id of courseName
 function setCourseInformation(course) {
     document.querySelector("#courseName").innerHTML = `${course.code} - ${course.title}`;
 }
 
+/* Create rows in an existing HTML table with an id of sections for each existing section
+This code separates the rendering using a map function from the template that builds a row. */
 function sectionTemplate(section) {
     return `<tr>
                 <td>${section.sectionNumber}</td>
