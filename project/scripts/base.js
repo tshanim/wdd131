241420800af4c7ade9eba1a628daf130488fa8ce
William Tshani <williamtshani@gmail.com>
2025-10-16 02:03:32 +0200

    Deleted some images

--- project/scripts/base.js
+++ project/scripts/base.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const navigation = document.querySelector('.navigation');
     const socialContent = `
         <a href="https://github.com/tshanim" target="_blank" title="GitHub"><img
-                class="icon github-logo" src="images/logos/github_logo.png"
+                class="icon github-logo" src="images/logos/github_logo.webp"
                 alt="GitHub icon" width="512" height="512"></a>
         <a href="https://x.com/btatshani" target="_blank" title="X"><img class="icon x-logo"
-                src="images/logos/X_logo.png" alt="X logo" width="960" height="960"></a>
+                src="images/logos/X_logo.webp" alt="X logo" width="960" height="960"></a>
         <a href="https://www.linkedin.com/in/william-tshani-1a254860/" target="_blank" title="LinkedIn"><img
-                class="icon linkedin-logo" src="images/logos/linkedin_logo.png"
+                class="icon linkedin-logo" src="images/logos/linkedin_logo.webp"
                 alt="LinkedIn icon" width="200" height="200"></a>
     `;
 
